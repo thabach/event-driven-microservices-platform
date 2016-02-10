d5fa6dc54f5c4d9837a10c5bcdbd1d441ae4c3a7
Marcel Birkner <marcel.birkner@codecentric.de>
2016-02-10 19:08:03 +0100

    added event driven microservices platform (edmp) seed job template

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Work in progress
+
+# Tools
+
+| *Tool* | *Link* | *Credentials* |
+| ------------- | ------------- | ------------- |
+| Jenkins | http://${docker-machine ip default}:18080/ | no login required |
+| SonarQube | http://${docker-machine ip default}:9000/ | admin/admin |
+| Nexus | http://${docker-machine ip default}:18081/nexus | admin/admin123 |
+
+# FAQ
+
+## Having problems downloading docker images?
+
+Error: Network timed out while trying to connect to https://index.docker.io/
+
+Solution:
+
+```
+# Add nameserver to DNS
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+
+# Restart the environment
+$ docker-machine restart default
+
+# Refresh your environment settings
+$ eval $(docker-machine env default)
+```
---
+++ jenkins/seedjobs/6-edmp-seed-job.groovy
@@ -0,0 +1,25 @@
+import groovy.json.JsonSlurper
+import hudson.FilePath
+import hudson.*
+
+println "############################################################################################################"
+println "Reading project configuration from json"
+
+hudson.FilePath workspace = hudson.model.Executor.currentExecutor().getCurrentWorkspace()
+File file = new File("${workspace}/src/main/groovy/project-configuration.json")
+def slurper = new JsonSlurper()
+def jsonText = file.getText()
+projects = slurper.parseText( jsonText )
+
+println "############################################################################################################"
+println "Iterating all projects"
+println ""
+
+projects.each {
+  println "############################################################################################################"
+  println ""
+  println "Creating Jenkins Jobs for Git Project: ${it.gitProjectName}"
+  println "- gitRepositoryUrl=${it.gitRepositoryUrl}"
+  println "- rootWorkDirectory=${it.rootWorkDirectory}"
+  println ""
+}
