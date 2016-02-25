93d5ef55363b4b8621c7afb4f4cb579e947e9e15
Marcel Birkner <marcelbirkner@Marcels-MacBook-Pro.local>
2016-02-25 22:22:11 +0100

    true

--- jenkins/jobs/dockerscripts/edmp-monitoring.sh
+++ jenkins/jobs/dockerscripts/edmp-monitoring.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker build -t edmp-monitoring .
-docker rm $(docker ps -a -q --filter="name=edmp-monitoring")
+docker rm $(docker ps -a -q --filter="name=edmp-monitoring") || true
 docker run --name="edmp-monitoring" --net=prodnetwork -p 10001:8080 edmp-monitoring
