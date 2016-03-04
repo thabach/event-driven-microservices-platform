f6c64d3bdde4372bb66d339a41e123a7e18c4bc3
Marcel Birkner <marcel.birkner@gmail.com>
2016-03-04 15:03:19 +0100

    Update edmpSeedJob.groovy scm trigger 5 min

--- jenkins/seedjobs/edmpSeedJob.groovy
+++ jenkins/seedjobs/edmpSeedJob.groovy
@@ -140,7 +140,7 @@ def createCIJob(def jobNamePrefix, def gitProjectName, def gitRepositoryUrl, def
       preBuildCleanup()
     }
     triggers {
-      scm('30/H * * * *')
+      scm('5/H * * * *')
       githubPush()
     }
     steps {
